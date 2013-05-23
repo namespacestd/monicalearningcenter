832aaf8bbd0c1be90236fc0c575ecf6af9ad3fbe
NamespaceSTD <ihasnamespacestd@gmail.com>
2013-05-22 18:40:24 -0700

    more basic layout

--- monicalearning/views.py
+++ monicalearning/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def our_program(request):
+	return render(request, 'our_program.html', { 'program' : True })
+
+def resources(request):
+	return render(request, 'resources.html', { 'resources' : True})
+
+def testimony(request):
+	return render(request, 'testimony.html')
+
+def register(request):
+	return render(request, 'register.html')
+
+def contact_us(request):
+	return render(request, 'contact_us.html')
